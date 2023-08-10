f3a0aa656c50f482a51cd9e8a05fc48a3bb562a2
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-08-10 21:54:54 +0200

    feat: server packages basic

---
+++ server/src/config/index.ts
@@ -0,0 +1,21 @@
+export const ENVIRONMENT = process.env.NODE_ENV;
+export const PORT = process.env.PORT;
+export const CORS_URL = process.env.CORS_URL;
+export const API_VERSION = process.env.API_VERSION;
+
+export const DB = {
+  name: process.env.DB_NAME || '',
+  host: process.env.DB_HOST || '',
+  port: process.env.DB_PORT || '',
+  minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || '5'),
+  maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || '10'),
+};
+
+export const TOKEN_INFO = {
+  accessTokenValidity: parseInt(process.env.ACCESS_TOKEN_VALIDITY_SEC || '0'),
+  refreshTokenValidity: parseInt(process.env.REFRESH_TOKEN_VALIDITY_SEC || '0'),
+  issuer: process.env.TOKEN_ISSUER || '',
+  audience: process.env.TOKEN_AUDIENCE || '',
+};
+
+export const LOG_DIRECTORY = process.env.LOG_DIR;
