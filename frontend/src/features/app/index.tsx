4abb31f4f3bca312949c4df634565737c4e535c3
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-10-06 17:25:32 +0200

    feat: install packages and initial config

---
+++ frontend/src/features/app/index.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+      <Toaster position="bottom-left" />
+      <div className="min-h-screen text-slate-600 bg-slate-900 xl:text-base sm:text-sm text-xs"></div>
+    </>
+  );
+}
+
+export default App;
