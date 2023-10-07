5078f37a5d7a6a00ff1d56afa3f2c50fa0931d71
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-10-07 12:03:38 +0200

    feat: axios and react query

---
+++ frontend/src/lib/axios/index.ts
@@ -0,0 +1,92 @@
+import { BASE_API_URL, LOGGING, NODE_ENV } from '../../config';
+import axios, { Method } from 'axios';
+
+const isLogEnabled = NODE_ENV !== 'production' && LOGGING == 'true';
+
+const instance = axios.create({
+  baseURL: BASE_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  maxContentLength: 5 * 1000 * 1000, // bytes => 5 MB,
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    if (isLogEnabled)
+      console.log(
+        'Network Request: ',
+        `${config.baseURL}${config.url}`,
+        config.method,
+      );
+    return config;
+  },
+  async (error) => {
+    if (isLogEnabled) console.error('Network Request Error: ', error);
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    return response;
+  },
+  async (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    if (isLogEnabled) console.error('Network Response Error: ', error);
+    return Promise.reject(error);
+  },
+);
+
+export type NetworkResponse<T extends object | null> = {
+  readonly statusCode: string;
+  readonly message: string;
+  readonly data?: T;
+};
+
+export type NetworkRequest<T extends object | null> = {
+  url: string;
+  method: Method;
+  data?: T;
+  params?: object;
+};
+
+export interface NetworkAuthRequest<T extends object | null>
+  extends NetworkRequest<T> {
+  headers?: { Authorization: string };
+}
+
+/**
+ * @T : Request Body Type
+ * @R : Response Body type
+ */
+export async function publicRequest<
+  T extends object | null,
+  R extends object | null,
+>(request: NetworkRequest<T>): Promise<NetworkResponse<R>> {
+  const { data } = await instance.request<NetworkResponse<R>>(request);
+  return data;
+}
+
+/**
+ * @T : Request Body Type
+ * @R : Response Body type
+ */
+export async function protectedRequest<
+  T extends object | null,
+  R extends object | null,
+>(request: NetworkRequest<T>, token: string): Promise<NetworkResponse<R>> {
+  try {
+    (request as NetworkAuthRequest<T>).headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const { data } = await instance.request<NetworkResponse<R>>(request);
+    return data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './features/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +19,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
