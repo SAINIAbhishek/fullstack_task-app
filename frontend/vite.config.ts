e097b5fe36a2f9afd59c4d17f2735f945ad9008d
Abhishek SAINI <saini.abhishek@hotmail.com>
2024-07-30 12:03:38 +0200

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_PORT');
+
+  return {
+    base: './',
+    envPrefix: 'VITE_',
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+      strictPort: true,
     },
-  },
+    preview: {
+      port: parseInt(env.VITE_PORT),
+      strictPort: true,
+      open: 'index.html',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
